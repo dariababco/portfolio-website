c3581f618ed9080e88644503d64c314b5a0e1d8c
dariababco <daria.babco@yahoo.de>
2019-03-18 22:40:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/*var header = document.getElementById("navi");
+var headernavs = header.getElementsByClassName("header-nav");
+for (var i = 0; i < headernavs.length; i++) {
+  headernavs[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}*/
+
